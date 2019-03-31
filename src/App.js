ef5d027bc0b2f7ef2269e4dac93f21b71ff3b63e
JamesVal <james.jo.val@gmail.com>
2019-03-31 10:43:15 -0700

    Continued development

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
+import Welcome from './Welcome';
 import InventoryData from './InventoryData';
 
 class App extends Component {
@@ -11,12 +13,23 @@ class App extends Component {
         <header className="App-header">
           Item Inventory
         </header>
+        <Router>
         <div className="links-container">
-        <span className="link">Inventory 1</span> | <span className="link">Inventory 2</span> | <span className="link">Inventory 3</span>
+        <Link to="/home" className="link"><span className="link">Home</span></Link> |&nbsp;
+        <Link to="/i1" className="link"><span className="link">Inventory 1</span></Link> |&nbsp;
+        <Link to="/i2" className="link"><span className="link">Inventory 2</span></Link> |&nbsp;
+        <Link to="/i3" className="link"><span className="link">Inventory 3</span></Link>
         </div>
         <div className="content">
-          <InventoryData iNumber={1}/>
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/home"/>}/>
+            <Route path="/home" component={Welcome}/>
+            <Route path="/i1" render={(props) => <InventoryData {...props} iNumber={1}/>}/>
+            <Route path="/i2" render={(props) => <InventoryData {...props} iNumber={2}/>}/>
+            <Route path="/i3" render={(props) => <InventoryData {...props} iNumber={3}/>}/>
+          </Switch>
         </div>
+        </Router>
       </div>
     );
   }
---
+++ src/Welcome.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import './Welcome.css';
+
+import dbClient from './api-db-cli';
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  render() {
+    return (
+      <div>Welcome!</div>
+    );
+  }
+}
+
+export default Welcome;
